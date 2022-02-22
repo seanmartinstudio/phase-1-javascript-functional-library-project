85de89f1284abf60205e008778103bfd840763c4
Sean Martin <sean@seanmartinstudio.com>
2022-02-21 16:37:54 -0800

    commit tree 137c0e9d8e4de4fc96bd2bdd4aab10afd5d6541a

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 function myEach(collection, alert) {
   for(let item in collection) {
-      console.log("item", item) 
+      alert(collection[item])
   }
-  
+  return collection
 }
+
+function myMap(collection, callback) {
+    let newArr = []
+    for(let item in collection) {
+        let value = callback(collection[item])
+        newArr.push(value)
+    }
+return newArr
+}
+
+// function myReduce(collection, callback, acc) {
+//     let total = 0
+//     for(let item in collection) {
+//         total = callback(acc, collection[item])
+//         console.log(total)
+        
+    
+// }
+// // return total
+// }
+
+function myFind(collection, predicate) {
+    collection.forEach(item => {
+        for(let key in item) {
+            console.log("Key=>", key, "Item=>", item)
+        }
+    })
+}
+    
+
+ 
